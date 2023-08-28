39ef62ec2e9f6fddc7e9b9775923ce1076adca25
Ivan Zyranau <ewancoder@gmail.com>
2023-08-28 15:16:25 +0300

    Create a minimal Library API that returns current date

---
+++ backend/library/TypingRealm.Library.Api/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using Microsoft.AspNetCore.Builder;
+
+var builder = WebApplication.CreateBuilder(args);
+
+var app = builder.Build();
+
+app.MapGet("/now", () => new
+{
+    Date = DateTime.UtcNow
+});
+
+await app.RunAsync();
