d437ee0d28492c5f238c7b19a9bbe449ab28b403
Ivan Zyranau <ewancoder@gmail.com>
2023-08-28 16:37:49 +0300

    Add Diagnostics controller with Now time endpoint
    Use AddControllers, MapControllers and [ApiController] to enable
    controllers in the assembly.

---
+++ backend/library/TypingRealm.Library.Api/Controllers/DiagnosticsController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TypingRealm.Library.Api.Controllers;
+
+[Route("diag")]
+public sealed class DiagnosticsController : ControllerBase
+{
+    [HttpGet]
+    [Route("now")]
+    public ActionResult Now()
+    {
+        return Ok(new
+        {
+            Now = DateTime.UtcNow
+        });
+    }
+}
--- backend/library/TypingRealm.Library.Api/Program.cs
+++ backend/library/TypingRealm.Library.Api/Program.cs
@@ -1,9 +1,23 @@
 ﻿using System;
 using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.DependencyInjection;
+
+// Applied on controllers, this attribute makes sure a number of API-related
+// things happen, for example: 400 is thrown when random string is passed as a
+// Guid input parameter
+[assembly:ApiController]
 
 var builder = WebApplication.CreateBuilder(args);
 
+// Adds controllers to DI. Required by MapControllers.
+builder.Services.AddControllers();
+
 var app = builder.Build();
+
+// Without this, controller classes are not being converted to endpoints (invisible).
+// This also requires a call to AddControllers on services.
+app.MapControllers();
 
 app.MapGet("/now", () => new
 {
